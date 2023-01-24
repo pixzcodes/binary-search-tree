77fb6546e315889c6c2ae9c3a7dd41d7a1eee56e
Pixz <tinycactusking@gmail.com>
2023-01-24 15:13:45 -0500

    Add pseudocode for project

---
+++ bbst.rb
@@ -0,0 +1,7 @@
+# Pseudo-code #
+# - First check for duplicates and remove them
+# - Then find the middle of the array and make it the root
+# - assign the left of the array to the left of the root
+# - repeat with recursion
+# - and same with the right array
+# - build out tree using recursion 
