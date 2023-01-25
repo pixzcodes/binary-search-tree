ef7957ee2b808651acfc4477cb8da4d539406d59
Pixz <tinycactusking@gmail.com>
2023-01-25 11:32:59 -0500

    Add Node class

--- bbst.rb
+++ bbst.rb
@@ -5,3 +5,16 @@
 # - repeat with recursion
 # - and same with the right array
 # - build out tree using recursion 
+
+class Node
+  attr_accessor :data, :left, :right
+  def initialize (data, left, right)
+    @data = data
+    @left = left
+    @right = right
+  end
+end
+
+
+
+
