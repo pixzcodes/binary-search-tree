8ae993c61d3649571bc6fca56321011fe96bf519
Pixz <tinycactusking@gmail.com>
2023-02-01 00:18:36 -0500

    Add insert, delet, and find methods to Tree class

--- bbst.rb
+++ bbst.rb
@@ -1,10 +1,10 @@
 
 class Node
   attr_accessor :data, :left, :right
-  def initialize (data, left, right)
+  def initialize (data, left = nil, right = nil)
     @data = data
-    @left = nil
-    @right = nil
+    @left = left
+    @right = right
   end
 end
 
@@ -28,7 +28,6 @@ class Tree
     root_node.right = build_tree(right)
 
     root_node
-
   end
 
   # This method came from a student on TheOdinProject Discord
@@ -38,7 +37,46 @@ class Tree
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
+  def insert (value, node = @root)
+    return nil if value == node.data # return nil if value already exists
 
+    if value < node.data
+      node.left.nil? ? node.left = Node.new(value) : insert(value, node.left)
+    else
+      node.right.nil? ? node.right = Node.new(value) : insert(value, node.right)
+    end
+  end
+  
+  def delete (value, node = @root)
+    return node if node.nil? 
+
+    if value < node.data
+      node.left = delete(value, node.left)
+    elsif value > node.data
+      node.right = delete(value, node.right)
+    else
+      # if the node has one or no child
+      return node.right if node.left.nil?
+      return node.left if node.right.nil?
+
+      far_left_node = far_left_leaf(node.right)
+      node.data = far_left_node.data
+      node.right = delete(far_left_node.data, node.right)
+    end
+    node
+  end
+
+  def far_left_leaf node
+    node = node.left until node.left.nil?
+
+    node
+  end
+
+  def find (value, node = @root)
+    return node if node.nil? || node.data == value
+
+    value < node.data ? find(value, node.left) : find(value, node.right)
+  end
 
 end
 
@@ -46,5 +84,20 @@ arr = [4,56,2,4,14,7,8,9,3,1]
 tree = Tree.new(arr)
 tree.pretty_print
 
+puts "insert"
+tree.insert(12)
+tree.pretty_print
 
+puts "delete 12"
+tree.delete(12)
+tree.pretty_print
+puts "delete 2"
+tree.delete(2)
+tree.pretty_print
+puts "delete 14"
+tree.delete(14)
+tree.pretty_print
+
+puts "find 4"
+p tree.find(4)
 
